b7a1a32e8f66319f12ef5d7c0f2403255fa7aed1
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-01-19 16:32:55 +0530

    feat:added responsiveness

--- studiotheme/assets/js/script.js
+++ studiotheme/assets/js/script.js
@@ -1,4 +1,16 @@
 const span = document.querySelector(".year");
+const menuBtn = document.querySelector(".menu-btn");
+const close = document.querySelector(".menu-btn--close");
+const menu = document.querySelector(".main-nav-mobile");
 const now = new Date();
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add("active");
+  document.querySelector("body").style.overflow = "hidden";
+});
+close.addEventListener("click", () => {
+  menu.classList.remove("active");
+  document.querySelector("body").style.overflow = "auto";
+});
 
 span.textContent = now.getFullYear();
