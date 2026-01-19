8c22fa7815ba3d082dcf37bfdc94e0204323f5f8
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-01-19 17:02:28 +0530

    feat:added menu function

--- studiotheme/assets/js/script.js
+++ studiotheme/assets/js/script.js
@@ -2,15 +2,23 @@ const span = document.querySelector(".year");
 const menuBtn = document.querySelector(".menu-btn");
 const close = document.querySelector(".menu-btn--close");
 const menu = document.querySelector(".main-nav-mobile");
+const mobileNav = document.querySelectorAll(".nav_link--mobile");
 const now = new Date();
 
-menuBtn.addEventListener("click", () => {
+function handelOpenMenu() {
   menu.classList.add("active");
   document.body.style.overflow = "hidden";
-});
-close.addEventListener("click", () => {
+}
+function handelCloseMenu() {
   menu.classList.remove("active");
   document.body.style.overflow = "auto";
+}
+menuBtn.addEventListener("click", handelOpenMenu);
+
+close.addEventListener("click", handelCloseMenu);
+
+mobileNav.forEach((li) => {
+  li.addEventListener("click", handelCloseMenu);
 });
 
 span.textContent = now.getFullYear();
