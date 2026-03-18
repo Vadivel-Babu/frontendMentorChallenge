cca067fd81280aaebe8ee29365985c5ee08f7a4d
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-03-18 11:53:16 +0530

    feat:folders and pages created

---
+++ blog/src/ui/Avatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Avatar = () => {
+  return (
+    <div className='w-12.5 h-12.5 bg-gray-300 rounded-full flex justify-center items-center overflow-hidden'>
+      <img className='w-full h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyFcvmHKM1inSOApmGcIYGT_XsE4KF9Pazuw&s" alt="img"/>
+    </div>
+  )
+}
+
+export default Avatar
---
+++ blog/src/ui/Input.jsx
@@ -0,0 +1,11 @@
+
+
+const Input = () => {
+  return (
+    <div>
+      <input type="text" placeholder='input' />
+    </div>
+  )
+}
+
+export default Input
