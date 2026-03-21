d1ea9a548070eeb437a73654cfb2f3e07519b6fd
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-03-21 12:03:11 +0530

    feat:all components are added

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -2,27 +2,18 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import SettingPage from "./pages/SettingPage";
 import CreatePostPage from "./pages/CreatePostPage";
-import { Avatar } from "@mantine/core";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Avatar
-        src={
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfUvEV4qKn_lxckDDd01lspzo2a9djhy4ZqQ&"
-        }
-        alt="Vitaly Rtishchev"
-        color="red"
-      >
-        VR
-      </Avatar>
       <Routes>
         <Route path="/" element="home" />
         <Route path="/login" element="login" />
         <Route path="/create-post" element={<CreatePostPage />} />
         <Route path="/setting" element={<SettingPage />} />
-        <Route path="/post" element="post" />
+        <Route path="/post" element={<PostPage />} />
       </Routes>
     </>
   );
