4b0d5c546fbece4ee48bd509628f05e09aa57f72
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-05-22 16:40:04 +0530

    feat:sidebar created

--- ecommdash/src/App.tsx
+++ ecommdash/src/App.tsx
@@ -1,20 +1,26 @@
-import { Baby } from "lucide-react";
 import "./App.css";
-import { Button } from "./components/ui/button";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import DashBoardPage from "./pages/DashBoardPage";
+import ProductsPage from "./pages/ProductsPage";
+import OrdersPage from "./pages/OrdersPage";
+import CreateProductPage from "./pages/CreateProductPage";
+import SettingPage from "./pages/SettingPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <>
-      <Button
-        className="bg-amber-400 hover:bg-black hover:text-blue-400"
-        size={"lg"}
-      >
-        hello
-        <Baby />
-      </Button>{" "}
+    <div className="flex flex-col md:flex-row">
       <Sidebar />
-    </>
+      <Routes>
+        <Route path="/" element={<DashBoardPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/create-product" element={<CreateProductPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- ecommdash/src/components/Sidebar.tsx
+++ ecommdash/src/components/Sidebar.tsx
@@ -1,5 +1,39 @@
+import { NavLink } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="bg-red-300 w-full md:max-w-max md:h-screen flex md:flex-col md:justify-between md:py-3 ">
+      <div className=" md:flex md:flex-col space-y-1">
+        <Avatar size="lg" className="m-auto">
+          <AvatarImage src="https://github.com/shadcn.pn" />
+          <AvatarFallback className="bg-orange-800 text-md text-white">
+            u
+          </AvatarFallback>
+        </Avatar>
+        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/"}>
+          dash
+        </NavLink>
+
+        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/products"}>
+          Products
+        </NavLink>
+        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/create-product"}>
+          Create Product
+        </NavLink>
+        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/orders"}>
+          Orders
+        </NavLink>
+        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/setting"}>
+          Setting
+        </NavLink>
+      </div>
+      <div className="px-3">
+        <Button>Logout</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
