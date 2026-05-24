2cd2e3c441f166ed11103757eddd2ce43a58ca85
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-05-24 12:28:09 +0530

    feat:navigation implemented

--- ecommdash/src/components/Sidebar.tsx
+++ ecommdash/src/components/Sidebar.tsx
@@ -1,36 +1,60 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const pathname = location.pathname.split("/")[1];
+  console.log(pathname);
+
   return (
-    <div className="bg-red-300 w-full md:max-w-max md:h-screen flex md:flex-col md:justify-between md:py-3 ">
-      <div className=" md:flex md:flex-col space-y-1">
+    <div className="bg-whiteblue text-lightblue capitalize md:text-lg w-full md:w-[10%]  md:h-screen flex md:flex-col md:justify-between md:py-3 ">
+      <div className=" md:flex md:flex-col">
+        <h1 className="md:text-center md:text-3xl text-primary-blue font-bold md:mb-4">
+          KaRt
+        </h1>
+        <div className="flex md:flex-col md:gap-3">
+          <NavLink
+            className={` ${pathname === "" && "bg-white text-[#2A4178] border-r-4 border-[#2A4178]"} hover:text-[#2A4178] px-3`}
+            to={"/"}
+          >
+            dash
+          </NavLink>
+
+          <NavLink
+            className={` ${pathname === "products" && "bg-white text-[#2A4178] border-r-4 border-[#2A4178]"} hover:text-[#2A4178]  px-3`}
+            to={"/products"}
+          >
+            Products
+          </NavLink>
+          <NavLink
+            className={` ${pathname === "create-product" && "bg-white text-[#2A4178] border-r-4 border-[#2A4178]"} hover:text-[#2A4178]  px-3`}
+            to={"/create-product"}
+          >
+            Create Product
+          </NavLink>
+          <NavLink
+            className={` ${pathname === "orders" && "bg-white text-[#2A4178] border-r-4 border-[#2A4178]"} hover:text-[#2A4178]  px-3`}
+            to={"/orders"}
+          >
+            Orders
+          </NavLink>
+          <NavLink
+            className={`${pathname === "setting" && "bg-white text-[#2A4178] border-r-4 border-[#2A4178]"} hover:text-[#2A4178]  px-3`}
+            to={"/setting"}
+          >
+            Setting
+          </NavLink>
+        </div>
+      </div>
+      <div className="px-3 md:flex md:flex-col justify-center md:gap-y-3">
         <Avatar size="lg" className="m-auto">
           <AvatarImage src="https://github.com/shadcn.pn" />
-          <AvatarFallback className="bg-orange-800 text-md text-white">
+          <AvatarFallback className="bg-primary-blue text-md text-white">
             u
           </AvatarFallback>
         </Avatar>
-        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/"}>
-          dash
-        </NavLink>
-
-        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/products"}>
-          Products
-        </NavLink>
-        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/create-product"}>
-          Create Product
-        </NavLink>
-        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/orders"}>
-          Orders
-        </NavLink>
-        <NavLink className={"hover:bg-yellow-500 px-1"} to={"/setting"}>
-          Setting
-        </NavLink>
-      </div>
-      <div className="px-3">
-        <Button>Logout</Button>
+        <Button className="m-auto bg-primary-blue">Logout</Button>
       </div>
     </div>
   );
