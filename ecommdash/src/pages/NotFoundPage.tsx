55354d060b571ccad54f21fb443cf4942cadc005
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-06-15 18:18:21 +0530

    feat:add product page created

--- ecommdash/src/pages/NotFoundPage.tsx
+++ ecommdash/src/pages/NotFoundPage.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="w-full flex items-center justify-center h-full">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl md:text-4xl font-bold">Page is Not Found</h1>
+        <Button onClick={() => navigate("/")}>Back To Home</Button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
