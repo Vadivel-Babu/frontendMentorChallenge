24cd63fbbf4cc5c90e8578a5ede8105542781698
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-06-16 18:17:22 +0530

    feat:product page created

--- ecommdash/src/pages/ProductsPage.tsx
+++ ecommdash/src/pages/ProductsPage.tsx
@@ -5,7 +5,12 @@ const ProductsPage = () => {
   return (
     <div>
       <Topbar />
-      <ProductCard />
+      <h1 className="text-center text-2xl my-3 md:text-4xl">Products</h1>
+      <div className="flex flex-wrap gap-2 max-w-250 justify-between m-auto">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i: number) => (
+          <ProductCard key={i} />
+        ))}
+      </div>
     </div>
   );
 };
