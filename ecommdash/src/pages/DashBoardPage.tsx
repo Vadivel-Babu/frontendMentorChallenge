91235563901d14605e7b49e4e341e4e152faa0c6
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-08-04 14:23:52 +0530

    feat:dashboard chart filter added

--- ecommdash/src/pages/DashBoardPage.tsx
+++ ecommdash/src/pages/DashBoardPage.tsx
@@ -2,6 +2,15 @@ import DashboardTable from "@/components/DashboardTable";
 import PostChart from "@/components/PostChart";
 import StatCard from "@/components/StatCard";
 import Topbar from "@/components/Topbar";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import UserChart from "@/components/UserChart";
 
 const DashBoardPage = () => {
@@ -14,9 +23,34 @@ const DashBoardPage = () => {
           <StatCard />
           <StatCard />
         </div>
-        <div className="flex justify-between my-2">
-          <PostChart />
-          <UserChart />
+        <div className="flex justify-between gap-2 my-2">
+          <div className="rounded-xl w-450 p-2 border">
+            <h1 className="text-md text-center my-1 md:text-xl">
+              Posts Posted
+            </h1>
+            <div className="flex gap-1 mb-1.5">
+              <label htmlFor="post filter">Filter by date</label>
+              <Select>
+                <SelectTrigger id="post filter" className="w-full max-w-48">
+                  <SelectValue placeholder="All" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Post</SelectLabel>
+
+                    <SelectItem value="k">joi</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <PostChart />
+          </div>
+          <div className="border rounded-xl p-2">
+            <h1 className="text-md text-center my-1 md:text-xl">
+              Posts Posted
+            </h1>
+            <UserChart />
+          </div>
         </div>
         <DashboardTable />
       </div>
