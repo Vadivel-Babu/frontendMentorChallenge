3831faa93dc9688583d1565e2e42e6e00c307a37
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-08-08 12:16:21 +0530

    feat:setting page design changed

--- ecommdash/src/pages/SettingPage.tsx
+++ ecommdash/src/pages/SettingPage.tsx
@@ -1,8 +1,21 @@
 import Topbar from "@/components/Topbar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Eye, EyeOff, ImageUp } from "lucide-react";
+import { useRef, useState } from "react";
 
 const SettingPage = () => {
+  const [isVisible, setVisible] = useState<number[]>([]);
+  const imgRef = useRef();
+
+  const handleToggleVisibility = (n: number) => {
+    const isNumberExists = isVisible.includes(n);
+    if (isNumberExists) {
+      setVisible(() => isVisible.filter((num) => num !== n));
+    } else {
+      setVisible([...isVisible, n]);
+    }
+  };
   return (
     <>
       <Topbar />
@@ -14,7 +27,13 @@ const SettingPage = () => {
           action=""
           className="border rounded-2xl p-2 space-y-1.5 max-w-100 mx-auto"
         >
-          <Input type="file" name="img" />
+          <div className="p-2">
+            <div className="border-2 rounded-2xl border-dashed p-2 cursor-pointer flex flex-col items-center justify-center gap-3">
+              <ImageUp size={30} />
+              <h1>Upload Picture</h1>
+            </div>
+            <Input ref={imgRef} type="file" name="img" className="hidden" />
+          </div>
           <Input type="text" placeholder="enter your name" name="name" />
           <Input
             type="text"
@@ -22,24 +41,54 @@ const SettingPage = () => {
             name="email"
             readOnly
           />
-          <Input
-            type="text"
-            placeholder="enter your email"
-            name="email"
-            readOnly
-          />
-          <Input
-            type="text"
-            placeholder="enter your email"
-            name="email"
-            readOnly
-          />
-          <Input
-            type="text"
-            placeholder="enter your email"
-            name="email"
-            readOnly
-          />
+          <div className="border rounded-lg flex">
+            <Input
+              type="password"
+              placeholder="enter your current password"
+              name="password"
+              className="border-none"
+            />
+            <Button
+              type="button"
+              size={"icon"}
+              className="bg-gray-200 text-black border-none rounded-bl-none rounded-tl-none"
+              onClick={() => handleToggleVisibility(1)}
+            >
+              {isVisible.includes(1) ? <Eye /> : <EyeOff />}
+            </Button>
+          </div>
+          <div className="border rounded-lg flex">
+            <Input
+              type="password"
+              placeholder="enter your new password"
+              name="password"
+              className="border-none"
+            />
+            <Button
+              type="button"
+              size={"icon"}
+              className="bg-gray-200 text-black border-none rounded-bl-none rounded-tl-none"
+              onClick={() => handleToggleVisibility(2)}
+            >
+              {isVisible.includes(2) ? <Eye /> : <EyeOff />}
+            </Button>
+          </div>
+          <div className="border rounded-lg flex">
+            <Input
+              type="password"
+              placeholder="enter your current password"
+              name="password"
+              className="border-none"
+            />
+            <Button
+              type="button"
+              size={"icon"}
+              className="bg-gray-200 text-black border-none rounded-bl-none rounded-tl-none"
+              onClick={() => handleToggleVisibility(3)}
+            >
+              {isVisible.includes(3) ? <Eye /> : <EyeOff />}
+            </Button>
+          </div>
 
           <Button className="bg-primary-blue text-white">update</Button>
         </form>
