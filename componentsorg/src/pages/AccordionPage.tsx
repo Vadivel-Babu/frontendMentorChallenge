2ee0b7975f53616a2366349f5dc38ec7d82d406c
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-08-15 18:06:13 +0530

    feat:accordion components created

---
+++ componentsorg/src/pages/AccordionPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Backbtn from "../components/Backbtn";
+
+const AccordionPage = () => {
+  return (
+    <div className="p-5">
+      <Backbtn />
+      <h1 className="text-2xl font-bold text-center mb-5">Accordion</h1>
+    </div>
+  );
+};
+
+export default AccordionPage;
