ddf22afea2f9225f47bd50cb8ce06af58c6ff907
Vadivel-Babu <vadivelbabu1996@gmail.com>
2026-08-17 22:04:18 +0530

    feat:refactoring layout

---
+++ componentsorg/src/components/Carasol.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Carasol = () => {
+  return <div>Carasol</div>;
+};
+
+export default Carasol;
--- componentsorg/src/pages/CaraselPage.tsx
+++ componentsorg/src/pages/CaraselPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Carasol from "../components/Carasol";
 
 const CaraselPage = () => {
-  return <div>CaraselPage</div>;
+  return (
+    <div>
+      <Carasol />
+    </div>
+  );
 };
 
 export default CaraselPage;
